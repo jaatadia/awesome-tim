6a46b8a4abbe85b9f5397bd8279950e6b88371e4
kadriya.dolunay <kadriya.dolunay@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-09-04 20:40:55 +0000

    Yaml cargar Juego (version Beta)

--- trunk/awesome-tim/awesome-tim/main.cpp
+++ trunk/awesome-tim/awesome-tim/main.cpp
@@ -1,5 +1,6 @@
 #include "Programa.h"
 #include "Juego.h"
+#include "ManejadorYaml.h"
 
 int main(int argc, char* argv[]){
 	
@@ -7,6 +8,7 @@ int main(int argc, char* argv[]){
 	Programa prog = Programa(argv[0]);//creo el programa
 	prog.correr();//lo corro (duhh)
 
+	ManejadorYaml::cargarJuego("archivoYaml.yaml", NULL, NULL);
 	
 	Juego juego = Juego(argv[0]);
 	while (juego.isRunning()&&(!juego.huboFallos())){
