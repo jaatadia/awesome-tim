77b2bd0aaf8d1bd5e51841b55a2842103c16a4a7
jaatadia <jaatadia@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-09-05 14:43:30 +0000

    agregue una carpeta release

--- awesome-tim/awesome-tim/main.cpp
+++ awesome-tim/awesome-tim/main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char* argv[]){
 	Programa prog = Programa(argv[0]);//creo el programa
 	prog.correr();//lo corro (duhh)
 
-	ManejadorYaml::cargarJuego("archivoYaml.yaml", NULL, NULL); //Linea para poder probar el YAML!!
-	
+	//ManejadorYaml::cargarJuego("archivoYaml.yaml", NULL, NULL); //Linea para poder probar el YAML!!
+	/*
 	Juego juego = Juego(argv[0]);
 	while (juego.isRunning()&&(!juego.huboFallos())){
 		juego.onEvent();
 		juego.onLoop();
 		juego.onRender();
 	}
-	
+	*/
 	return 0;
 }
