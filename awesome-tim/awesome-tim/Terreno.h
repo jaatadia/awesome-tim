3fc5f6a712e34804c6923d7a890ed8e72b9e3791
florencia.a.etcheverry <florencia.a.etcheverry@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-09-05 16:43:58 +0000

    agregar y eliminar de la lista de figuras en el Terreno.

--- awesome-tim/awesome-tim/Terreno.h
+++ awesome-tim/awesome-tim/Terreno.h
@@ -3,6 +3,13 @@
 #include "Imprimible.h"
 #include "Validable.h"
 #include "Cambiable.h"
+#include <list>
+
+/* terreno es el que contiene todas las figuras
+y tiene las funciones necesarias para que le roten algun objeto le eliminen y dibuje todos los objetos en una superficie
+
+mover una figura tambien?? 
+*/
 
 class Terreno: public Imprimible,public Validable,public Cambiable
 {
@@ -10,11 +17,19 @@ private:
 	int ancho,alto;
 	Imagen* img;
 	Superficie* sup;
+	std::list<Figura*> figuras;
 
 public:
 	Terreno(int ancho,int alto);
 	~Terreno(void);
-	void setFondo(Imagen* img);
+
 	Superficie* getImpresion();
+
+	void setFondo(Imagen* img);
+
+	void agregarFigura(Figura* fig);
+	void rotarFigura(Figura* fig);
+	void eliminarFigura(Figura* fig);
+
 
 };
