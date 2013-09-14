bfac9d8a1e017af0792d5e8811becbc05242a53f
kadriya.dolunay <kadriya.dolunay@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-09-14 21:40:57 +0000

    cargador Yaml la parte del terreno casi completa, falta agregar lo de los numeros de linea y algunos tipos de figura y dimensiones mas

--- trunk/awesome-tim/awesome-tim/CargadorYAML.h
+++ trunk/awesome-tim/awesome-tim/CargadorYAML.h
@@ -7,11 +7,20 @@
 #include <string.h>
 #include <vector>
 
+#include "Constantes.h"
+
 #include "BotoneraController.h"
 #include "Terreno.h"
 #include "Posicion.h"
+
 #include "Cuadrado.h"
 #include "Circulo.h"
+
+#include "Figura.h"
+#include "FiguraCuadrada.h"
+#include "FiguraCircular.h"
+#include "FiguraPoligonal.h"
+#include "FiguraTriangular.h"
 
 class CargadorYaml{
 public:
@@ -24,7 +33,7 @@ private:
 	static void cargar_terreno(const YAML::Node& nodoTerreno,Terreno* terreno);
 	static void cargar_figuras(const YAML::Node& listaFiguras, Terreno* terreno);
 	static Dimension* obtener_dimension(const YAML::Node& dimension,const char* tipo_dimension);
-	static Figura* cargarFigura(const char* tipo_figura,const char* imagen,int posX,int posY,Dimension* dimension);
+	static Figura* cargarFigura(const char* tipo_figura,const char* ID,Dimension* dimension);
 	static Dimension* crearDimension(const YAML::Node& dimension,double angulo,double posX,double posY, const char* tipo_dimension);
 	static Dimension* crearCuadrado(const YAML::Node& dimension, double angulo,double posX,double posY);
 	static Dimension* crearCirculo(const YAML::Node& dimension, double angulo,double posX,double posY);
