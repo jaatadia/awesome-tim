01e4649c8c2372fff21a4c74afd20eb2a6950850
jaatadia <jaatadia@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-09-17 13:04:06 +0000

    otro pequeño arreglo en conetenedor

--- trunk/awesome-tim/awesome-tim/Contenedor.cpp
+++ trunk/awesome-tim/awesome-tim/Contenedor.cpp
@@ -11,12 +11,17 @@ void Contenedor::putMultimedia(const char* ID,void* medio){
 		cont->mapa["NONE"] = new Imagen();
 	}
 	
-	if(cont->mapa.find(ID)== cont->mapa.end()){
+	Imagen* img = (Imagen*) medio;
+	if (img->huboFallos()){
+		std::cout<< "--una imagen fallo\n";
+		delete img;
+		return;
+	}else if(cont->mapa.find(ID)== cont->mapa.end()){
 		std::cout<< "--A contenedor se le pide que se guarde algo con este ID: "<<ID<<"\n";
 		cont->mapa[ID]=medio;
 	}else{
 		std::cout<< "--El medio: "<<ID<<" ya se encuentra en el contenedor\n";
-		delete medio;
+		delete img;
 	}
 }
 
