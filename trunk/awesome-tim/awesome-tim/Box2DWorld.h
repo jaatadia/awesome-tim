6852c240608c63a82b696148f82bc5b35a24d9ad
matsebman <matsebman@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-09-27 03:33:42 +0000

    - Implementado Box2D - Implementado "Circulo" (Bola con restitucion)

---
+++ trunk/awesome-tim/awesome-tim/Box2DWorld.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Box2D/Box2D.h"
+#include "Figura.h"
+#include "Constantes.h"
+#include "Circulo.h"
+
+class Box2DWorld
+{
+private:
+	Box2DWorld(void);
+	b2World * mundo;
+	float tiempoStep;
+	int velIteracion, posIteracion;
+public:
+	Box2DWorld(float fuerzaX, float fuerzaY);
+	void setFrecuenciaActualizacion(float tiempoStep, int velIteracion, int posIteracion);
+	void actualizar();
+	void actualizar(Figura * figura);
+	void agregarFigura(Figura * figura);
+	void cambiarParametros(Figura * figura);
+	void eliminarFigura(Figura * figura);
+	~Box2DWorld(void);
+};
