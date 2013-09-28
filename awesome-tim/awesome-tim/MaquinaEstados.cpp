03a9db3b07abf48702fd799fa2ab969f7b88c21b
jaatadia <jaatadia@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-09-28 22:27:29 +0000

    nunca subi la maquina de estado XD

---
+++ awesome-tim/awesome-tim/MaquinaEstados.cpp
@@ -0,0 +1,9 @@
+#include "MaquinaEstados.h"
+
+MaquinaEstados::MaquinaEstados(void)
+{
+}
+
+MaquinaEstados::~MaquinaEstados(void)
+{
+}
---
+++ awesome-tim/awesome-tim/MaquinaEstados.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class MaquinaEstados
+{
+public:
+	virtual void salir()=0;
+	virtual void editor()=0;
+	virtual void play()=0;
+};
