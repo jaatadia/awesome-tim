0649c9b22c803d34f4e58ade2b519d02b51ce693
florencia.a.etcheverry <florencia.a.etcheverry@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-10-01 16:59:36 +0000

    fotitos

--- trunk/awesome-tim/awesome-tim/Box2DWorld.cpp
+++ trunk/awesome-tim/awesome-tim/Box2DWorld.cpp
@@ -12,7 +12,7 @@ Box2DWorld::Box2DWorld(float fuerzaX, float fuerzaY)
 	groundBodyDef.position.Set(50.0f, 100.0f);
 	b2Body* groundBody = (this->mundo)->CreateBody(&groundBodyDef);
 	b2PolygonShape groundBox;
-	groundBox.SetAsBox(50.0f, 1.0f);
+	groundBox.SetAsBox(50.0f, 0.0f);
 	groundBody->CreateFixture(&groundBox, 0.0f);
 
 }
