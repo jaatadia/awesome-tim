dceeaf3b70d2961855c3a4c404f0dc3752248ec3
jaatadia <jaatadia@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-10-09 13:45:49 +0000

    cambie una configuracion del visual studio para que capture las excepciones de windows tambien

--- trunk/awesome-tim/awesome-tim/VistaLinea.cpp
+++ trunk/awesome-tim/awesome-tim/VistaLinea.cpp
@@ -10,29 +10,38 @@ VistaLinea::~VistaLinea(void)
 }
 
 void VistaLinea::dibujar(Superficie* canvas){
-	int calcX1;
-	int calcY1;
-	int calcX2;
-	int calcY2;
-
-	if(linea->fig1){
-		double x,y;
-		linea->fig1->posAtableCorrea(&x,&y);
-		linea->setPunto1(x,y);
-	}
-	calcX1 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasX(((DLinea*)linea->getDimension())->x1));
-	calcY1 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasY(((DLinea*)linea->getDimension())->y1));
 	
-	if(linea->fig2){
-		double x,y;
-		linea->fig2->posAtableCorrea(&x,&y);
-		linea->setPunto2(x,y);
+	try{
+		int calcX1;
+		int calcY1;
+		int calcX2;
+		int calcY2;
+
+		if(linea->fig1){
+			double x,y;
+			linea->fig1->posAtableCorrea(&x,&y);
+			linea->setPunto1(x,y);
+		}
+		
+		calcX1 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasX(((DLinea*)linea->getDimension())->x1));
+		calcY1 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasY(((DLinea*)linea->getDimension())->y1));
+			
+		if(linea->fig2){
+			double x,y;
+			linea->fig2->posAtableCorrea(&x,&y);
+			linea->setPunto2(x,y);
+		}
+		calcX2 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasX(((DLinea*)linea->getDimension())->x2));
+		calcY2 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasY(((DLinea*)linea->getDimension())->y2));
+		
+
+		int calcAncho = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasX(((DLinea*)linea->getDimension())->ancho));
+
+		int desfasaje = calcAncho;
+		canvas->dibujarLinea(calcX1+desfasaje,calcY1+desfasaje,calcX2+desfasaje,calcY2+desfasaje,calcAncho);
+		canvas->dibujarLinea(calcX1-desfasaje,calcY1-desfasaje,calcX2-desfasaje,calcY2-desfasaje,calcAncho);
+		//canvas->dibujarLinea(calcX1,calcY1,calcX2,calcY2,calcAncho);
+	}catch(...){
+		//std::cout<<"se produjo una falla\n\a";
 	}
-	calcX2 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasX(((DLinea*)linea->getDimension())->x2));
-	calcY2 = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasY(((DLinea*)linea->getDimension())->y2));
-	
-
-	int calcAncho = int(EscalasDeEjes::getInstance()->getCantidadUnidadesFisicasX(((DLinea*)linea->getDimension())->ancho));
-
-	canvas->dibujarLinea(calcX1,calcY1,calcX2,calcY2,calcAncho);
 }
