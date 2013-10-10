e1c0e60c7bd2bc7417e2cf847179e26a2313b83b
jaatadia <jaatadia@4a0d6ecb-281d-3ed1-78c5-56459388ea3f>
2013-10-10 21:53:07 +0000

    balancin que hiso flor

--- trunk/awesome-tim/awesome-tim/Balancin.h
+++ trunk/awesome-tim/awesome-tim/Balancin.h
@@ -25,13 +25,25 @@ public:
 	Balancin(const char* ID,double posX, double posY, double angulo);	//pos y angulo son de la tabla!
 	~Balancin(void);
 	static void calcularPosPuntas(double* posXizq,double* posXder, double* posY, double posX_Tabla, double posY_Tabla);
-	bool atar(double posX, double posY);
-	bool desatar(double posX, double posY);
-	bool esAtable(double* posX, double* posY);
+	//bool atar(double posX, double posY);
+	//bool desatar(double posX, double posY);
+	//bool esAtable(double* posX, double* posY);
 
 	Figura* clonar();
 	int getTipoDimension();
 	int getTipoFigura();
+
+	//se le pasa la poicion en la que esta queriendo atar y devuelve el numero de posicion atable mas cercano(-1 es que no habia)
+	virtual int esAtableSoga(double x,double y);
+	//se le pasa un numero de posicion atable y devuelve la posicion x e y de donde se ata por mas que ya este atado
+	virtual void posAtableSoga(int numero,double* x,double* y);
+	virtual void atarSoga(int numero);//le dice que ate una soga en su pocicion atable numero
+	virtual void desatarSoga(int numero);//le dice que desate una soga de su posicion atable numero
+	
+	Figura* getTabla();
+	Figura* getPuntaIzq();
+	Figura* getPuntaDer();
+
 
 //	bool choqueConFigura(Figura* fig);
 };
